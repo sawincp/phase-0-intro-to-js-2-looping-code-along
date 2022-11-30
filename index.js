5ce63eeb70ce18adad45e636b573bd6568b74525
Corey Sawin <sawincp@gmail.com>
2022-11-30 12:28:49 -0500

    commit tree 3aa258c1b6afdd84c4f2f5b4c457ba8c9df15ba7

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added to the bowl!`);
+    }
+}
+
+wrapGifts(gifts);
+
+
+
+const names = ["Corey", "Megan", "Scott"];
+const action = "birthday"
+
+function writeCards(names,action){
+    const message = [];
+    for (let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${action} gift!`)
+    }
+    return(message);
+}
+writeCards(names, action)
+
+function countDown(){
+    let count = 10;
+    while (count >= 0){
+        console.log(count--);
+    }
+}
+countDown()
